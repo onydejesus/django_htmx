9693924d86c73111868d24522a4194e2094b83e4
onorio <onorio@catalpa.io>
2023-07-12 16:29:47 +0900

    making add books with htmx

--- htmx_app/forms.py
+++ htmx_app/forms.py
@@ -6,6 +6,9 @@ class BookForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = (
-            'title',
-            'number_of_pages'
+            'title', 'number_of_pages'
         )
+        widgets = {
+            "title": forms.TextInput(attrs={'class': 'form-control'}),
+            "number_of_pages": forms.TextInput(attrs={'class': 'form-control'}),
+        }
